18cd9ee53c81784baace58f427b21ab26b0867c4
Meredith Bloom <meredithbloom@Merediths-iMac.local>
2022-06-14 14:27:06 -0400

    initial files. redux demo includes simple redreducer, store, subscriber

---
+++ redux-demo.js
@@ -0,0 +1,14 @@
+const redux = require('redux')
+
+
+const counterReducer = (state, action) => {
+    return {
+        counter: state.counter + 1
+    }
+}
+
+const store = redux.configureStore(counterReducer);
+
+const counterSubscriber = () => {
+    store.getState()
+}
